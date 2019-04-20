6ca9c8ff98d151db41df940f0c474ef2ab145567
goki sakaguchi <goki0524@gmail.com>
2019-04-20 17:40:53 +0900

    Fix todoRedux

--- redux/todoRedux.js
+++ redux/todoRedux.js
@@ -1,52 +1,65 @@
-import {  
-  combineReducers,
-  createStore,
-} from 'redux'
-
-
-
-// actions.js
-export const deleteTodo = () => ({  
-  type: 'DELETE_TODO',
-  name: ''
-})
-
-export const setTodo = name => ({  
-  type: 'ADD_TODO',
-  name: name,
-})
-
-INITIAL_STATE = {
-  name: 'Todo 1'
+const types = {
+  ADD_ITEM: 'ADD_ITEM',
+  REMOVE_ITEM: 'REMOVE_ITEM',
+  TOGGLE_ITEM_COMPLETED: 'TOGGLE_ITEM_COMPLETED',
+  REMOVE_COMPLETED: 'REMOVE_COMPLETED',
 }
 
+export const actionCreators = {
+  addItem: (item) => {
+    return {type: types.ADD_ITEM, payload: item}
+  },
+  removeItem: (index) => {
+    return {type: types.REMOVE_ITEM, payload: index}
+  },
+  toggleItemCompleted: (index) => {
+    return {type: types.TOGGLE_ITEM_COMPLETED, payload: index}
+  },
+  removeCompleted: (item) => {
+    return {type: types.REMOVE_COMPLETED, payload: item}
+  },
+}
 
-// reducers.js
-const reducer = (state = INITIAL_STATE, action) => {  
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {...state, name: action.name}
-    case 'DELETE_TODO':
-      return {...state, name: ''}
-    default:
+const initialState = {
+  items: [],
+}
+
+export const reducer = (state = initialState, action) => {
+  const {type, payload} = action
+  const {items} = state
+
+  switch(type) {
+    case types.ADD_ITEM: {
+      return {
+        ...state,
+        items: [{label: payload, completed: false}, ...items],
+      }
+    }
+    case types.REMOVE_ITEM: {
+      return {
+        ...state,
+        items: items.filter((item, i) => i !== payload),
+      }
+    }
+    case types.TOGGLE_ITEM_COMPLETED: {
+      return {
+        ...state,
+        items: items.map((item, i) => {
+          if (i === payload) {
+            return {label: item.label, completed: !item.completed}
+          }
+          return item
+        }),
+      }
+    }
+    case types.REMOVE_COMPLETED: {
+      return {
+        ...state,
+        items: items.filter((item) => !item.completed)
+      }
+    }
+    default: {
       return state
+    }
   }
 }
-
-export const reducers = combineReducers({  
-  todo: reducer
-})
-
-
-// store.js
-export const store = createStore(reducers)
-
-
-console.log("Hello!")
-console.log(store.getState)
-
-console.table(store.getState)
-
-console.log(typeof store.getState)
-
-console.log(store)
